06e9e27fadf9f3effce8fd48098fe90d04910318
Lokesh Poreddy <your-email@example.com>
2026-06-21 21:06:44 +0530

    sucker

---
+++ GCD.java
@@ -0,0 +1,15 @@
+public class GCD {
+    public void GCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println("GCD: " + a);
+    }
+
+    public static void main(String[] args) {
+        GCD sol = new GCD();
+        sol.GCD(48, 18); 
+    }
+}
