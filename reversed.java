85de13128f175ac9e118fd9ac01a17dff40b534d
Lokesh Poreddy <your-email@example.com>
2026-06-25 11:38:46 +0530

    suckerrr

---
+++ reversed.java
@@ -0,0 +1,24 @@
+public class reversed {
+    public void reverse(int[] arr, int n) {
+        int l = 0;
+        int r = n -1;
+        while(l < r){
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+
+        l++;
+        r--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int n = arr.length;
+        reversed obj = new reversed();
+        obj.reverse(arr, n);
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
